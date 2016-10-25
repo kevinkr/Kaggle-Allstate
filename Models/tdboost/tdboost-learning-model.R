b79c92b765b4629ec1dde8a7c40cae88f13a854e
kevinkr <kevinkr@yahoo.com>
2016-10-25 18:51:33 -0500

    refactor code

--- Models/tdboost/tdboost-learning-model.R
+++ Models/tdboost/tdboost-learning-model.R
@@ -7,6 +7,28 @@ source("Code/1-load data.R")
 train <- train %>% mutate_each(funs(factor), starts_with("cat"))
 test <- test %>% mutate_each(funs(factor), starts_with("cat"))
 
+# Harmonize factor levels
+for (attr in colnames(train))
+{
+  
+}
+
+# 
+# for(attr in colnames(training))
+# {
+#   if (is.factor(training[[attr]]))
+#   {
+#     new.levels <- setdiff(levels(training[[attr]]), levels(testing[[attr]]))
+#     if ( length(new.levels) == 0 )
+#     { print(paste(attr, '- no new levels')) }
+#     else
+#     {
+#       print(c(paste(attr, length(new.levels), 'of new levels, e.g.'), head(new.levels, 2)))
+#       levels(testing[[attr]]) <- union(levels(testing[[attr]]), levels(training[[attr]]))
+#     }
+#   }
+# }
+
 set.seed(3456)
 trainIndex <- createDataPartition(train$loss, p = 0.8, list=FALSE, times=1)
 subTrain <- train[trainIndex,]
@@ -30,10 +52,16 @@ test$cat92[1770] <- "D"
 test <- test %>% mutate_each(funs(factor), starts_with("cat"))
 
 #fit <- TDboost(loss ~. , data=trainSet, cv.folds=5, n.trees=300, interaction.depth = 20)
-fit <- TDboost(loss ~. , data=trainSet, cv.folds=5, n.trees=100, interaction.depth = 20)
+fit <- TDboost(loss ~. , 
+               data=trainSet, 
+               cv.folds=5, 
+               n.trees=100, 
+               interaction.depth = 20, 
+               distribution = list(name="EDM",alpha=1.5))
 
 # print out optimal iteration number
 best.iter <- TDboost.perf(fit, method="test")
+
 
 # check performance using 5-fold cross-validation
 best.iter <- TDboost.perf(fit,method="cv")
